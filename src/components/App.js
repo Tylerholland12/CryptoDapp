05ae013c47c58cc74ba07ed8fe10ddd8c07fbce0
Tyler_Holland <tyler.holland@students.makeschool.com>
2020-10-15 00:24:23 -0500

    [update] UI/UX

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,53 @@
 import React, { Component } from 'react';
-import logo from '../logo.png';
+import Web3 from 'web3'
+import Navbar from './Navbar'
+// import logo from '../logo.png';
 import './App.css';
 
 class App extends Component {
+
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+    // console.log(this.state.account)
+
+    const ethbalance = await web3.eth.getBalance(this.state.account)
+    this.setState({ ethbalance })
+    console.log(this.state.ethBalance)
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '',
+      ethBalance: '0'
+    }
+  }
+
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="/"
-            rel="noopener noreferrer"
-          >
-            YO
-          </a>
-        </nav>
+       <Navbar account={this.state.account}/>
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
@@ -23,11 +56,11 @@ class App extends Component {
                   href="/"
                   rel="noopener noreferrer"
                 >
-                  <img src={logo} className="App-logo" alt="logo" />
+                  {/* <img src={logo} className="App-logo" alt="logo" /> */}
                 </a>
-                <h1>YO</h1>
+                <h1>This is where the swap occurs</h1>
                 <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
+                  {/* Edit <code>src/components/App.js</code> and save to reload. */}
                 </p>
                 <a
                   className="App-link"
