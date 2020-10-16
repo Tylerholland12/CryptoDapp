4c18fa51f9a2f2a248939df13b21bb1d0c9b29b5
Tyler_Holland <tyler.holland@students.makeschool.com>
2020-10-15 21:01:13 -0500

    Updates

---
+++ precache-manifest.c74a6d93aae05c326ec40e9dbf207369.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "176ea98100ca0d2752fd10455a212898",
+    "url": "/CryptoDapp/index.html"
+  },
+  {
+    "revision": "ffaa3516a153a6b3db69",
+    "url": "/CryptoDapp/static/css/2.c4608a58.chunk.css"
+  },
+  {
+    "revision": "97465a75628850ffeeb0",
+    "url": "/CryptoDapp/static/css/main.d850c1a7.chunk.css"
+  },
+  {
+    "revision": "ffaa3516a153a6b3db69",
+    "url": "/CryptoDapp/static/js/2.984f890b.chunk.js"
+  },
+  {
+    "revision": "79a10e03b12a0bcc9828d59fc879d698",
+    "url": "/CryptoDapp/static/js/2.984f890b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97465a75628850ffeeb0",
+    "url": "/CryptoDapp/static/js/main.2e2af5b0.chunk.js"
+  },
+  {
+    "revision": "910701e54f1185d2cc66",
+    "url": "/CryptoDapp/static/js/runtime-main.4cae6ea6.js"
+  }
+]);
